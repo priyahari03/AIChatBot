819c990ff22605607a3c71e93ec8f3db44053675
HariPriya <priyahari.h03@gmail.com>
2025-01-18 18:51:46 -0600

    ChatBot Application

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Chat from './components/Chat/chat';
+
+function App() {
+    return (
+        <div className="App">
+            <h1 style={{ textAlign: 'center' }}>Chatbot Application</h1>
+            <Chat />
+        </div>
+    );
+}
+
+export default App;
